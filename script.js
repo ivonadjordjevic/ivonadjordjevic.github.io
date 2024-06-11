18da67294cabed8681b119e9bd45b62d7a0e4189
unknown <ivona2402@gmail.com>
2024-06-11 19:19:27 +0200

    fix

--- script.js
+++
@@ -1,9 +0,0 @@
-$(".slike").on("click", function(){
-  console.log("USAO")
-  $("#full-image").attr("src", $(this).attr("src"));
-  $('#image-viewer').show();
-});
-
-$("#image-viewer .close").click(function(){
-  $('#image-viewer').hide();
-});
